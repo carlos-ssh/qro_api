0ba14d074f5f8ed979bd301aa2964ac3b41e315a
Carlos <carlos.anezeh@gmail.com>
2022-03-16 17:55:31 -0600

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :users
       root to: "home#index"
     end
   end
