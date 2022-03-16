73e0c835dfc4cb7a7fd30e3efd5998c0f8efbc33
Carlos <carlos.anezeh@gmail.com>
2022-03-16 14:30:45 -0600

    Add omiauth authentication with social media.

---
+++ app/controllers/api/v1/home_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::HomeController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    render json: {
+      message: 'Welcome to Queretaro API!',
+    }
+  end
+end
