121cea3296aa963cb997eb6082f553efb84f18d9
Carlos <carlos.anezeh@gmail.com>
2022-03-18 12:20:55 -0600

    add validations for user session

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :sessions, only: [:create]
       resources :properties
-      resources :users
       root to: "home#index"
     end
   end
