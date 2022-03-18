00a6eeb35ad4f49cfdcd19838db6566638c653e3
Carlos <carlos.anezeh@gmail.com>
2022-03-18 12:31:27 -0600

    add registration rules for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :sessions, only: [:create]
+      resources :registrations, only: [:create]
       resources :properties
+
       root to: "home#index"
     end
   end
