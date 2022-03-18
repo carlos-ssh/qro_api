98bfcdc4f16cbf7d30c4f86652ad500661209306
Carlos <carlos.anezeh@gmail.com>
2022-03-18 16:56:27 -0600

    Fix issues

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-Rails.application.config.session_store :cookie_store,
-      key: '_qro_api_session',
-      domain: "https://qro-api.herokuapp.com/api/v1"
-=======
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_qro_api_session', domain: "https://qro-api.herokuapp.com/api/v1/sessions"
+      Rails.application.config.session_store :cookie_store, key: '_qro_api_session', domain: "https://qro-api.herokuapp.com/api/v1/sessions"
 else
-  Rails.application.config.session_store :cookie_store, key: '_qro_api_session'
+      Rails.application.config.session_store :cookie_store, key: '_qro_api_session'
 end
->>>>>>> session_store
